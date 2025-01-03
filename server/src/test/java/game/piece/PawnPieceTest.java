b29f43e43ac6f78aeecd2dd6be06c0876c911ba5
D1149311 <126126863+D1149311@users.noreply.github.com>
2025-01-03 23:22:44 +0800

    Queen&PawnTest

--- server/src/test/java/game/piece/PawnPieceTest.java
+++ server/src/test/java/game/piece/PawnPieceTest.java
@@ -6,49 +6,47 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-//Cannot read field "color" because "pawn" is null
+//Test
 class PawnPieceTest {
     @Test
-    void Test() {
+    void TestBlack() {
         ChessPiece[][] empty_board = new ChessPiece[ChessUtils.BOARD_SIZE][ChessUtils.BOARD_SIZE];
         List<Position> expect = new ArrayList<>();
-        for(int x = 1; x < 8; x++){
-            int y = x;
-            expect.add(new Position(x,y,false));
-        }
-        empty_board[0][1] = new PawnPiece(PieceColor.WHITE);
+        expect.add(new Position(0,2,false));
+        expect.add(new Position(0,3,false));
+        empty_board[1][0] = new PawnPiece(PieceColor.BLACK);
         List<Position> result = PawnPiece.getPossibleMoves(0,1,empty_board, null);
         PositionTest.assertPosition(expect,result);
     }
-//    @Test
-//    void TestEat() {
-//        ChessPiece[][] empty_board = new ChessPiece[ChessUtils.BOARD_SIZE][ChessUtils.BOARD_SIZE];
-//        List<Position> expect = new ArrayList<>();
-//        empty_board[1][1] = new PawnPiece(PieceColor.WHITE);
-//        empty_board[0][0] = new PawnPiece(PieceColor.BLACK);
-//        List<Position> result = PawnPiece.getPossibleMoves(0,0,empty_board,null);
-//        PositionTest.assertPosition(expect,result);
-//    }
-//    @Test
-//    void TestBlock(){
-//        ChessPiece[][] empty_board = new ChessPiece[ChessUtils.BOARD_SIZE][ChessUtils.BOARD_SIZE];
-//        List<Position> expect = new ArrayList<>();
-//        empty_board[3][3] = new PawnPiece(PieceColor.WHITE);
-//        empty_board[0][0] = new PawnPiece(PieceColor.WHITE);
-//        for(int x = 1; x < 3; x++){
-//            int y = x;
-//            expect.add(new Position(x,y,false));
-//        }
-//        List<Position> result = PawnPiece.getPossibleMoves(0,0,empty_board,null);
-//        PositionTest.assertPosition(expect,result);
-//    }
-//    @Test
-//    void TestCorner(){
-//        ChessPiece[][] empty_board = new ChessPiece[ChessUtils.BOARD_SIZE][ChessUtils.BOARD_SIZE];
-//        List<Position> expect = new ArrayList<>();
-//        empty_board[1][1] = new PawnPiece(PieceColor.WHITE);
-//        empty_board[0][0] = new PawnPiece(PieceColor.WHITE);
-//        List<Position> result = PawnPiece.getPossibleMoves(0,0,empty_board,null);
-//        PositionTest.assertPosition(expect,result);
-//    }
+    @Test
+    void TestWhite() {
+        ChessPiece[][] empty_board = new ChessPiece[ChessUtils.BOARD_SIZE][ChessUtils.BOARD_SIZE];
+        List<Position> expect = new ArrayList<>();
+        expect.add(new Position(0,5,false));
+        expect.add(new Position(0,4,false));
+        empty_board[6][0] = new PawnPiece(PieceColor.WHITE);
+        List<Position> result = PawnPiece.getPossibleMoves(0,6,empty_board, null);
+        PositionTest.assertPosition(expect,result);
+    }
+    @Test
+    void TestEat() {
+        ChessPiece[][] empty_board = new ChessPiece[ChessUtils.BOARD_SIZE][ChessUtils.BOARD_SIZE];
+        List<Position> expect = new ArrayList<>();
+        empty_board[2][1] = new PawnPiece(PieceColor.WHITE);
+        empty_board[1][0] = new PawnPiece(PieceColor.BLACK);
+        expect.add(new Position(1,2,true));
+        expect.add(new Position(0,2,true));
+        expect.add(new Position(0,3,true));
+        List<Position> result = PawnPiece.getPossibleMoves(0,1,empty_board,null);
+        PositionTest.assertPosition(expect,result);
+    }
+    @Test
+    void TestBlock(){
+        ChessPiece[][] empty_board = new ChessPiece[ChessUtils.BOARD_SIZE][ChessUtils.BOARD_SIZE];
+        List<Position> expect = new ArrayList<>();
+        empty_board[2][0] = new PawnPiece(PieceColor.BLACK);
+        empty_board[1][0] = new PawnPiece(PieceColor.BLACK);
+        List<Position> result = PawnPiece.getPossibleMoves(0,1,empty_board,null);
+        PositionTest.assertPosition(expect,result);
+    }
 }
