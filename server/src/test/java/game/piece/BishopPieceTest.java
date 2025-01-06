be6ccde3e57be77630cc9d280d8380cf83400208
D1149311 <126126863+D1149311@users.noreply.github.com>
2025-01-06 17:51:48 +0800

    KnightQueenPiTest

--- server/src/test/java/game/piece/BishopPieceTest.java
+++ server/src/test/java/game/piece/BishopPieceTest.java
@@ -2,11 +2,15 @@ package game.piece;
 
 import game.*;
 import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.Null;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 class BishopPieceTest {
+
     @Test
     void testEmptyBoard() {
         ChessPiece[][] board = new ChessPiece[ChessUtils.BOARD_SIZE][ChessUtils.BOARD_SIZE];
@@ -67,17 +71,48 @@ class BishopPieceTest {
         PositionTest.assertPosition(expected, result);
     }
 
-//    @Test
-//    void testBoardEdges() {
-//        ChessPiece[][] board = new ChessPiece[ChessUtils.BOARD_SIZE][ChessUtils.BOARD_SIZE];
-//        List<Position> expected = new ArrayList<>();
-//        board[0][7] = new BishopPiece(PieceColor.WHITE);
-//        for (int i = 1; i < ChessUtils.BOARD_SIZE; i++) {
-//            if (0 + i < ChessUtils.BOARD_SIZE && 7 - i >= 0) {
-//                expected.add(new Position(0 + i, 7 - i, false));
-//            }
-//        }
-//        List<Position> result = BishopPiece.getPossibleMoves(0, 7, board);
-//        PositionTest.assertPosition(expected, result);
-//    }
+    @Test
+    void testFullBoardScenario() {
+        ChessPiece[][] board = new ChessPiece[ChessUtils.BOARD_SIZE][ChessUtils.BOARD_SIZE];
+        for (int i = 0; i < ChessUtils.BOARD_SIZE; i++) {
+            for (int j = 0; j < ChessUtils.BOARD_SIZE; j++) {
+                board[i][j] = new PawnPiece(PieceColor.WHITE); // 填滿棋盤
+            }
+        }
+        board[4][4] = new BishopPiece(PieceColor.WHITE); // 放置主教
+        List<Position> expected = new ArrayList<>(); // 主教無法移動
+        List<Position> result = BishopPiece.getPossibleMoves(4, 4, board);
+        PositionTest.assertPosition(expected, result);
+    }
+
+    @Test
+    void testSurroundedByEnemies() {
+        ChessPiece[][] board = new ChessPiece[ChessUtils.BOARD_SIZE][ChessUtils.BOARD_SIZE];
+        board[4][4] = new BishopPiece(PieceColor.WHITE);
+        board[5][5] = new PawnPiece(PieceColor.BLACK);
+        board[3][3] = new RookPiece(PieceColor.BLACK);
+        board[5][3] = new KnightPiece(PieceColor.BLACK);
+        board[3][5] = new QueenPiece(PieceColor.BLACK);
+
+        List<Position> expected = List.of(
+                new Position(5, 5, true), // Down-right
+                new Position(3, 3, true), // Up-left
+                new Position(5, 3, true), // Down-left
+                new Position(3, 5, true)  // Up-right
+        );
+        List<Position> result = BishopPiece.getPossibleMoves(4, 4, board);
+        PositionTest.assertPosition(expected, result);
+    }
+
+    @Test
+    void testEdgeBoundary() {
+        ChessPiece[][] board = new ChessPiece[ChessUtils.BOARD_SIZE][ChessUtils.BOARD_SIZE];
+        board[7][0] = new BishopPiece(PieceColor.WHITE);
+        List<Position> expected = new ArrayList<>();
+        for (int i = 1; i < ChessUtils.BOARD_SIZE; i++) {
+            expected.add(new Position(i, 7-i, false));
+        }
+        List<Position> result = BishopPiece.getPossibleMoves(0, 7, board);
+        PositionTest.assertPosition(expected, result);
+    }
 }
--- server/src/test/java/game/piece/KnightPieceTest.java
+++ server/src/test/java/game/piece/KnightPieceTest.java
@@ -8,84 +8,92 @@ import java.util.ArrayList;
 import java.util.List;
 
 class KnightPieceTest {
-    @Test
-    void testEmptyBoard() {
-        ChessPiece[][] board = new ChessPiece[ChessUtils.BOARD_SIZE][ChessUtils.BOARD_SIZE];
-        List<Position> expected = List.of(
-                new Position(5, 6, false),
-                new Position(3, 6, false),
-                new Position(5, 2, false),
-                new Position(3, 2, false),
-                new Position(6, 5, false),
-                new Position(6, 3, false),
-                new Position(2, 5, false),
-                new Position(2, 3, false)
-        );
-        board[4][4] = new KnightPiece(PieceColor.BLACK);
-        List<Position> result = KnightPiece.getPossibleMoves(4, 4, board);
-        PositionTest.assertPosition(expected, result);
-    }
 
     @Test
-    void testCaptureOpponentPiece() {
+    void testSurroundedByEnemies() {
         ChessPiece[][] board = new ChessPiece[ChessUtils.BOARD_SIZE][ChessUtils.BOARD_SIZE];
+        board[4][4] = new KnightPiece(PieceColor.WHITE);
+        board[6][5] = new PawnPiece(PieceColor.BLACK);
+        board[6][3] = new PawnPiece(PieceColor.BLACK);
+        board[5][6] = new PawnPiece(PieceColor.BLACK);
+        board[5][2] = new PawnPiece(PieceColor.BLACK);
+        board[3][6] = new PawnPiece(PieceColor.BLACK);
+        board[3][2] = new PawnPiece(PieceColor.BLACK);
+        board[2][5] = new PawnPiece(PieceColor.BLACK);
+        board[2][3] = new PawnPiece(PieceColor.BLACK);
+
         List<Position> expected = List.of(
-                new Position(5, 6, false),
-                new Position(3, 6, false),
-                new Position(5, 2, false),
-                new Position(3, 2, false),
-                new Position(6, 5, false),
-                new Position(6, 3, false),
+                new Position(6, 5, true),
+                new Position(6, 3, true),
+                new Position(5, 6, true),
+                new Position(5, 2, true),
+                new Position(3, 6, true),
+                new Position(3, 2, true),
                 new Position(2, 5, true),
-                new Position(2, 3, false)
+                new Position(2, 3, true)
         );
-        board[4][4] = new KnightPiece(PieceColor.WHITE);
-        board[2][5] = new KnightPiece(PieceColor.BLACK);
         List<Position> result = KnightPiece.getPossibleMoves(4, 4, board);
         PositionTest.assertPosition(expected, result);
     }
 
     @Test
-    void testBlockedBySameColorPiece() {
+    void testFullBoardScenario() {
         ChessPiece[][] board = new ChessPiece[ChessUtils.BOARD_SIZE][ChessUtils.BOARD_SIZE];
-        board[4][4] = new KnightPiece(PieceColor.WHITE);
-        board[6][5] = new KnightPiece(PieceColor.WHITE);
-        board[6][3] = new KnightPiece(PieceColor.WHITE);
-        board[5][6] = new KnightPiece(PieceColor.WHITE);
-        board[5][2] = new KnightPiece(PieceColor.WHITE);
-        board[3][6] = new KnightPiece(PieceColor.WHITE);
-        board[3][2] = new KnightPiece(PieceColor.WHITE);
-        board[2][5] = new KnightPiece(PieceColor.WHITE);
-        board[2][3] = new KnightPiece(PieceColor.WHITE);
-
-        List<Position> expected = new ArrayList<>();
+        for (int i = 0; i < ChessUtils.BOARD_SIZE; i++) {
+            for (int j = 0; j < ChessUtils.BOARD_SIZE; j++) {
+                board[i][j] = new PawnPiece(PieceColor.WHITE); // 填滿棋盤
+            }
+        }
+        board[4][4] = new KnightPiece(PieceColor.WHITE); // 放置騎士
+        List<Position> expected = new ArrayList<>(); // 騎士無法移動
         List<Position> result = KnightPiece.getPossibleMoves(4, 4, board);
         PositionTest.assertPosition(expected, result);
     }
 
     @Test
-    void testCornerPosition() {
+    void testKnightOnEdgeWithMixedPieces() {
         ChessPiece[][] board = new ChessPiece[ChessUtils.BOARD_SIZE][ChessUtils.BOARD_SIZE];
-        board[0][0] = new KnightPiece(PieceColor.WHITE);
+        board[0][7] = new KnightPiece(PieceColor.WHITE);
+        board[2][6] = new PawnPiece(PieceColor.WHITE); // 阻擋
+        board[1][5] = new PawnPiece(PieceColor.BLACK); // 可吃
+
+        List<Position> expected = List.of(
+                new Position(5, 1, true) // 吃子
+        );
+        List<Position> result = KnightPiece.getPossibleMoves(7, 0, board);
+        PositionTest.assertPosition(expected, result);
+    }
+
+    @Test
+    void testNoPossibleMoves() {
+        ChessPiece[][] board = new ChessPiece[ChessUtils.BOARD_SIZE][ChessUtils.BOARD_SIZE];
+        board[4][4] = new KnightPiece(PieceColor.WHITE);
+        board[6][5] = new PawnPiece(PieceColor.WHITE);
+        board[6][3] = new PawnPiece(PieceColor.WHITE);
+        board[5][6] = new PawnPiece(PieceColor.WHITE);
+        board[5][2] = new PawnPiece(PieceColor.WHITE);
+        board[3][6] = new PawnPiece(PieceColor.WHITE);
+        board[3][2] = new PawnPiece(PieceColor.WHITE);
+        board[2][5] = new PawnPiece(PieceColor.WHITE);
+        board[2][3] = new PawnPiece(PieceColor.WHITE);
+
+        List<Position> expected = new ArrayList<>(); // 騎士無法移動
+        List<Position> result = KnightPiece.getPossibleMoves(4, 4, board);
+        PositionTest.assertPosition(expected, result);
+    }
+
+    @Test
+    void testKnightOnBoundaryCenter() {
+        ChessPiece[][] board = new ChessPiece[ChessUtils.BOARD_SIZE][ChessUtils.BOARD_SIZE];
+        board[0][4] = new KnightPiece(PieceColor.BLACK);
 
         List<Position> expected = List.of(
                 new Position(2, 1, false),
-                new Position(1, 2, false)
+                new Position(3, 2, false),
+                new Position(6, 1, false),
+                new Position(5, 2, false)
         );
-        List<Position> result = KnightPiece.getPossibleMoves(0, 0, board);
-        PositionTest.assertPosition(expected, result);
-    }
-
-    @Test
-    void testBoundaryEdges() {
-        ChessPiece[][] board = new ChessPiece[ChessUtils.BOARD_SIZE][ChessUtils.BOARD_SIZE];
-        board[0][7] = new KnightPiece(PieceColor.BLACK);
-
-        List<Position> expected = List.of(
-                new Position(2, 6, false),
-                new Position(1, 5, false)
-        );
-        List<Position> result = KnightPiece.getPossibleMoves(0, 7, board);
+        List<Position> result = KnightPiece.getPossibleMoves(4, 0, board);
         PositionTest.assertPosition(expected, result);
     }
 }
